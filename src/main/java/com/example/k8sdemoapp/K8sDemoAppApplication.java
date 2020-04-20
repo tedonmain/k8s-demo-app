34a0e81b79bf220855c892f1626053199b0c31b8
tedonmain <tedonmain@gmail.com>
2020-04-20 13:38:09 -0700

    initial k8s-demo-app

---
+++ src/main/java/com/example/k8sdemoapp/K8sDemoAppApplication.java
@@ -0,0 +1,21 @@
+package com.example.k8sdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class K8sDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sDemoAppApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		String toReturn = "Hola everyone, Wowzers!!!... I'm here inside a container in a Kubernetes cluster!!!";
+		return toReturn;
+	}
+}
